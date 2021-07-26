e82d9ac347763d5aee98dd7e009412d951cd9020
TheWookersquatch <ksprettygurl@gmail.com>
2021-07-26 14:32:34 -0600

    week 6 complete

--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ChallengesWithTestsMark8
 {
@@ -37,22 +38,81 @@ namespace ChallengesWithTestsMark8
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            if (num <= 1 || num == null)
+                return false; 
+            if (num == 2)
+            {
+                return true;
+            }
+
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+            
+            return true;
         }
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
+            List<char> unique = new List<char>();
+            for (int i = 0 ; i < str.Length ; i++)
+            {
+                int x = 0;
+                for (int t =0 ; t < str.Length ; t++)
+                {
+                    if (str[i] == str[t] && i != t)
+                    {
+                        x++;
+                    }
+                }
+                if( x == 0)
+                {
+                    unique.Add(str[i]);
+                }
+            }
+
+            if (unique.Count == 0 || unique == null)
+            {
+                return -1;
+            }
+            var n = unique[unique.Count - 1];
+            return str.IndexOf(n);
         }
 
         public int MaxConsecutiveCount(int[] numbers)
         {
-            throw new NotImplementedException();
+            int x = 0;      //tracks current number's count
+            int y = 0;      //tracks largest consecutive count
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    x++;
+                }
+                else
+                {
+                    if (x > 0 && x >= y)
+                    {
+                        y = x+1;
+                    }
+                    x = 0;
+                }
+
+                if (i == numbers.Length - 1 && x >= y)
+                {
+                    y = x + 1;
+                }
+            }
+
+            return y;
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            double[] x = new double[0];
+            return elements == null || elements.Count == 0 ? x : elements.Where((c) => (elements.IndexOf(c) % n) - (n-1) == 0).ToArray();
         }
     }
 }
